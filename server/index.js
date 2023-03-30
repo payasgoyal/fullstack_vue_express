17325852877861d567ae9c5dd230d772868bfe50
Payas Goyal <payasgoyal0224@gmail.com>
2023-03-30 20:22:39 +0800

    added static folder and prepared SPA

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ app.use(cors());
 const posts = require('./routes/api/posts');
 app.use('/api/posts', posts);
 
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+    //static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //Handle SPA
+    app.get(/.* /, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
